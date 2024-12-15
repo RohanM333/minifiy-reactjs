079f3c2a33a355c9b90a2d0a02e5f4fd28a8f7da
Rohan Majrikar <62887858+RohanM333@users.noreply.github.com>
2024-12-15 23:16:40 +0530

    Update config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -1,20 +1,21 @@
 // npm install react-app-rewired customize-cra terser-webpack-plugin --save-dev
 
-const { override, addWebpackPlugin } = require('customize-cra');
+const { override, addBabelPreset, addWebpackPlugin } = require('customize-cra');
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = override(
+  addBabelPreset('minify'),
   (config) => {
     if (config.optimization) {
-      config.optimization.minimizer = [
+      config.optimization.minimizer.push(
         new TerserPlugin({
           terserOptions: {
             compress: {
               drop_console: true, // Remove console logs
             },
           },
-        }),
-      ];
+        })
+      );
     }
     return config;
   }
